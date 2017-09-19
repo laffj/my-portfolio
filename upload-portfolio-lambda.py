40098b4c21d4f169c76b799fedb428ad5b505dfe
John A. Laffey <john.laffey@pega.com>
2017-09-18 17:52:00 -0700

    code to set mimetypes

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -38,7 +38,8 @@ def lambda_handler(event, context):
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={
+                'Content-Type' : mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print "Deployment Complete!"
         # push the confirmation email
